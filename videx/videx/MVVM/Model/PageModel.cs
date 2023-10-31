f236e868a70c9665792b70db939cd922087e961f
PSY <125935704+PSY21801028@users.noreply.github.com>
2023-10-31 20:37:55 +0900

    videx_ui(WPF)
    first commint (~비디오 인덱싱 진행중)

---
+++ videx/videx/MVVM/Model/PageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videx.MVVM.Model
+{
+    public class PageModel
+    {
+        public int SettingValue { get; set; }
+        public string DataPick { get; set; }
+        public bool OutlierDetecion { get; set; }
+    }
+}
---
+++ videx/videx/MVVM/ViewModels/NavigationViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using videx.MVVM;
+using System.Windows.Input;
+
+namespace videx.MVVM.ViewModels
+{
+    class NavigationViewModel : ViewModelBase
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand SettingCommand { get; set; }
+        public ICommand AnalysisCommand { get; set; }
+
+        private void Setting(object obj) => CurrentView = new SettingViewModel();
+        private void Analysis(object obj) => CurrentView = new AnalysisViewModel();
+
+
+        public NavigationViewModel()
+        {
+            SettingCommand = new RelayCommand(Setting);
+            AnalysisCommand = new RelayCommand(Analysis);
+
+            // Startup Page
+            CurrentView = new SettingViewModel();
+        }
+    }
+}
